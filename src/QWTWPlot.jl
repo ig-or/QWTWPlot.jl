d5b041854a620cfdb8d8f6abd949592b5d62dfc3
ig-or <19777752+ig-or@users.noreply.github.com>
2021-08-10 12:27:38 -0700

    a few comments changed

--- src/QWTWPlot.jl
+++ src/QWTWPlot.jl
@@ -429,15 +429,15 @@ function qfigure()
 end;
 
 """
-qclipgrp(gr) 
+	qclipgrp(gr) 
 
-set current 'clip group' \
-gr is the ID of this new group \
+set current 'clip group'.
+
+gr is the ID of this new group. 
 all (new) subsequent plots/figires will belong to this group. if press 'clip' button, 
 then if will work only for plots from the same group. 
 
 useful in case some of the plots have another time range.
-
 """
 function qclipgrp(gr) 
 	global qwtwClipGroupH, qwtwLibHandle, started
